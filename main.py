a5e2061d31e1f5b326c3ba2d577924df058908ab
saarthakpant <91788405+saarthakpant@users.noreply.github.com>
2024-08-21 21:45:15 +0530

    Draw Screen

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 import pygame
+from constants import *
+
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+      
+      
+      
+      
+      
+      
+        
+    # print("Starting asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__== "__main__":
     main()
