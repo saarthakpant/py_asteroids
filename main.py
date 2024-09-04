5b6a7dfc4494d9e08f941154a97689992fd0ed87
saarthakpant <91788405+saarthakpant@users.noreply.github.com>
2024-09-04 21:27:42 +0530

    Cleaner

--- main.py
+++ main.py
@@ -7,15 +7,29 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+    
+
+
     while True:
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+
+        for obj in updatable:
+            obj.update(dt)
+        
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         
 
--- player.py
+++ player.py
@@ -19,3 +19,24 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(),2)
         
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.positon += forward * PLAYER_SPEED * dt
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_LEFT]:
+            self.rotate(-dt)
+        if keys[pygame.K_RIGHT]:
+            self.rotate(dt)
+        if keys[pygame.K_UP]:
+            self.move(dt)
+        if keys[pygame.K_DOWN]:
+            self.move(-dt)  
+            
+ 
